672d7f8897cf1991a76c8c232d7abdba9c1b0883
MohamedEID12345 <ydm5384@gmail.com>
2022-09-20 12:59:40 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  calc = '';
+  result = '0';
+  getAction(item:any){
+    this.calc += item
+    console.log(item)
+  }
+
+  getResult(){
+this.result=eval(this.calc)
+  }
+
+  getReset(){
+    this.calc=''
+    this.result='0'
+  }
 }
